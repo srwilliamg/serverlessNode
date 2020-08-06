73d00558c22c6f56f32229ab23293d3cb3ad8bda
William Salazar <srwilliamg@gmail.com>
2020-08-05 21:07:48 -0500

    adding schedule taks

---
+++ severlessNode/src/handlers/processAuctions.js
@@ -0,0 +1,13 @@
+// import AWS from 'aws-sdk';
+// import middleware from '../lib/middleware'
+// import createError from 'http-errors';
+
+// const dynamodb =  new AWS.DynamoDB.DocumentClient();
+
+async function processAuctions(event, context) {
+  console.log(`Processing auctions!`);
+}
+
+export const handler = processAuctions;
+
+
